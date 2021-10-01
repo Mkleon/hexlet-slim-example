a2648df0f7e3dcb1c21f997081ff941144d36d22
Kirill Miklashevsky <klmiklashevsky@gmail.com>
2021-10-01 16:13:49 +0300

    add: new route for /users

--- public/index.php
+++ public/index.php
@@ -10,8 +10,15 @@ $app->addErrorMiddleware(true, true, true);
 $app->get(
     '/', 
     function ($request, $response) {
-        $response->getBody()->write('Welcome to Slim!');
+        $response->getBody()->write("Welcome to Slim!");
         return $response;
+    }
+);
+
+$app->get(
+    '/users',
+    function ($request, $response) {
+        return $response->write('GET users');
     }
 );
 
